387891e9ab3164664e90f6a638b7dd918a6c9c58
Damien Matabon <damien.matabon@gmail.com>
2022-02-18 22:27:32 +0100

    fix deprecations

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -15,7 +15,7 @@ class Configuration implements ConfigurationInterface
     /**
      * {@inheritdoc}
      */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('redking_font_awesome_picker');
         if (!method_exists($treeBuilder, 'getRootNode')) {
